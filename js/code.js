9e4901a43a68f4ee32af48b6ffbb38b8741bc322
SebastianSandovalB <sebaa9719@gmail.com>
2020-11-17 22:47:06 -0400

    cambios2

--- js/code.js
+++ js/code.js
@@ -1,23 +1,76 @@
-// Obtenemos el button y lo almacenamos en una variable llamada "btn"
 var btn = document.getElementById("btn");
-/* Obtenemos el div que muestra el resultado y lo almacenamos en una variable llamada "resultado" */ var resultado = document.getElementById("resultado")
-/* Obtenemos los dos input y los almacenamos en variables "inputUno" y "inputDos" */
+var btnSumar = document.getElementById("btnSumar");
+var btnRestar = document.getElementById("btnRestar");
+var btnMultiplicar = document.getElementById("btnMultiplicar");
+var btnDividir = document.getElementById("btnDividir");
+
+
+
+var resultado = document.getElementById("resultado"); 
+var resultado2 = document.getElementById("resultado2")
+
 var inputUno = document.getElementById("input-uno");
 var inputDos = document.getElementById("input-dos");
-// Añadimos el evento click a la variable "btn"
-btn.addEventListener("click", function () {
-    /* Obtenemos el valor de cada input accediendo a su atributo "value" */
-    var min = inputUno.value;
-    var max = inputDos.value;
-/* Llamamos a una función que permite realizar la suma de los números y los mostramos al usuario" */ 
-resultado.innerHTML = random(min, max);
-});
-/* Función que retorna la suma de dos números */
+var inputUnoCalc = document.getElementById("input-uno-calc");
+var inputDosCalc = document.getElementById("input-dos-calc");
+
+
 function suma(n1, n2) {
-    // Es necesario aplicar parseInt a cada número
+   
     return parseInt(n1) + parseInt(n2);
 }
-function random(min, max) {
-    // Es necesario aplicar parseInt a cada número
-    return Math.floor(Math.random()*max)+min;
+function resta(n1, n2) {
+
+    return parseInt(n1) - parseInt(n2);
 }
+function multiplica(n1, n2) {
+
+    return parseInt(n1) * parseInt(n2);
+}
+function divide(n1, n2) {
+
+    return parseInt(n1) / parseInt(n2);
+}
+function random(min, max) {
+    
+    return Math.floor(Math.random() * parseInt(max-1)) + parseInt(min);
+}
+
+
+btn.addEventListener("click", function () {
+
+    var min = inputUno.value;
+    var max = inputDos.value;
+
+    resultado.innerHTML = random(min, max); 
+});
+
+btnSumar.addEventListener("click", function () {
+
+    var n1 = inputUnoCalc.value;
+    var n2 = inputDosCalc.value;
+
+    resultado2.innerHTML = suma(n1, n2);
+});
+
+btnRestar.addEventListener("click", function () {
+
+    var n1 = inputUnoCalc.value;
+    var n2 = inputDosCalc.value;
+
+    resultado2.innerHTML = resta(n1, n2);
+});
+btnMultiplicar.addEventListener("click", function () {
+
+    var n1 = inputUnoCalc.value;
+    var n2 = inputDosCalc.value;
+
+    resultado2.innerHTML = multiplica(n1, n2);
+});
+btnDividir.addEventListener("click", function () {
+
+    var n1 = inputUnoCalc.value;
+    var n2 = inputDosCalc.value;
+
+    resultado2.innerHTML = divide(n1, n2);
+});
